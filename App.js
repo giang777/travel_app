6eba26e804e26044027e8b1281dae7fdd8393b50
khanhhehehe <117183565+khanhhehehe@users.noreply.github.com>
2023-07-13 18:12:09 +0700

    checkcommit

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import TabsNavigation from "./src/routes/tabs-navigation";
 import store from "./src/redux/store";
 import { Provider } from "react-redux";
 
-
 export default function App() {
   return (
     <Provider store={store}>
