48ac5f7c9d92be5e6cfd301f475b23f6edf3deb4
Nguyendat2109 <dat21092003@gmail.com>
2023-08-06 19:58:09 +0700

    udUI_2

--- src/screens/search/index.js
+++ src/screens/search/index.js
@@ -1,14 +1,46 @@
 import React, {useState} from 'react';
-import {Text, View, TextInput, TouchableOpacity, Image, StyleSheet,ScrollView,FlatList } from 'react-native';
+import {Text, View, TextInput, TouchableOpacity, Image, StyleSheet,ScrollView,FlatList,CheckBox } from 'react-native';
 import styles from './styles';
 import styleshome from '../home/styles'
 import { arrOptions, arrFakeData } from '../home/fakeData';
 import { RenderItemListHorizontal,RenderItemListVertical } from '../../components/renderList';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {ColorAssets, IconAssets} from "../../utils/app-assets";
+import Modal from 'react-native-modal';
+import {arrCity, arrResults, arrStar, optionsCheckBox} from "./fakedataSearch";
+import { Ionicons } from '@expo/vector-icons';
 
 
 const SearchBar = () => {
+    const [isModalVisible, setModalVisible] = useState(false);
+
+    const toggleModal = () => {
+        setModalVisible(!isModalVisible);
+    };
+    const [indexOptions, setindexOptions] = useState(1);
+    // const [boxchecked, setchecked] = useState(optionsCheckBox);
+    // const toggleCheckbox = (itemId) => {
+    //     setchecked((prevData) =>
+    //         prevData.map((item) =>
+    //             item.id === itemId ? { ...item, isChecked: !item.isChecked } : item
+    //         )
+    //     );
+    // };
+    // const renderCheckboxItem = ({ item }) => (
+    //     <View style={styles.checkboxContainer}>
+    //         <CheckBox
+    //             value={item.isChecked}
+    //             onValueChange={() => toggleCheckbox(item.id)}
+    //         />
+    //         <Text style={styles.labelcb}>{item.label}</Text>
+    //     </View>
+    // );
+
+    // const [priceRange, setPriceRange] = useState([0, 100]);
+    //
+    // const onPriceRangeChange = (values) => {
+    //     setPriceRange(values);
+    // };
     return (
 
             <View style={styles.searchBar}>
@@ -20,9 +52,89 @@ const SearchBar = () => {
                 <TouchableOpacity style={styles.iconButton}>
                     <Image style={styles.icon} source={{uri:'https://cdn-icons-png.flaticon.com/128/54/54481.png'}} />
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.iconButton}>
+                <TouchableOpacity style={styles.iconButton} onPress={toggleModal}>
                     <Image style={styles.icon}source={{ uri: 'https://cdn-icons-png.flaticon.com/128/3024/3024539.png' }} />
                 </TouchableOpacity>
+                {/*//Bottom dialog*/}
+                <Modal
+                    isVisible={isModalVisible}
+                    style={styles.modal}
+                    backdropOpacity={0.5}
+                    onBackdropPress={toggleModal}
+                    swipeDirection={['down']} // Có thể thay đổi hướng vuốt để đóng dialog
+                    onSwipeComplete={toggleModal}
+                >
+                    <View style={styles.modalContent}>
+                        <Text style={styles.label}>Country</Text>
+                        {/*Options*/}
+                        <View style={styles.ViewFilter}>
+
+                            <ScrollView  horizontal={true} style={styles.viewOptions} showsVerticalScrollIndicator={false}>
+                                {
+                                    arrCity.map((item, index) => {
+                                        return (
+                                            <TouchableOpacity
+                                                style={[styles.itemOptions, indexOptions === item.id ? styles.itemOptions_isActive : styles.itemOptions_noActive]}
+                                                onPress={() => { setindexOptions(item.id) }}
+                                                key={index}
+                                            >
+                                                <Text style={[{fontWeight: 'bold', fontSize: 16},indexOptions === item.id ? { color: "white" } : { color: ColorAssets.greenColor }]}>{item.title}</Text>
+                                            </TouchableOpacity>
+                                        )
+                                    })
+                                }
+                            </ScrollView>
+                        </View>
+                        <Text style={styles.label}>Results</Text>
+                        <View style={styles.ViewFilter}>
+
+                            <ScrollView  horizontal={true} style={styles.viewOptions} showsVerticalScrollIndicator={false}>
+                                {arrResults.map((item, index) => {
+                                        return (
+                                            <TouchableOpacity
+                                                style={[styles.itemOptions, indexOptions === item.id ? styles.itemOptions_isActive : styles.itemOptions_noActive]}
+                                                onPress={() => { setindexOptions(item.id) }}
+                                                key={index}
+                                            >
+                                                <Text style={[{fontWeight: 'bold', fontSize: 16},indexOptions === item.id ? { color: "white" } : { color: ColorAssets.greenColor }]}>{item.title}</Text>
+                                            </TouchableOpacity>
+                                        )
+                                    })
+                                }
+                            </ScrollView>
+                        </View>
+                        <Text style={styles.label}>Star</Text>
+                        <View style={styles.ViewFilter}>
+
+                            <ScrollView  horizontal={true} style={styles.viewOptions} showsVerticalScrollIndicator={false}>
+                                {
+                                    arrStar.map((item, index) => {
+                                    return (
+                                        <TouchableOpacity
+                                            style={[styles.itemOptions, indexOptions === item.id ? styles.itemOptions_isActive : styles.itemOptions_noActive]}
+                                            onPress={() => { setindexOptions(item.id) }}
+                                            key={index}
+                                        >
+                                            <Text style={[{fontWeight: 'bold', fontSize: 16},indexOptions === item.id ? { color: "white" } : { color: ColorAssets.greenColor }]}>{item.title}</Text>
+                                        </TouchableOpacity>
+                                    )
+                                })
+                                }
+                            </ScrollView>
+                        </View>
+
+                        {/*checkbox*/}
+                        {/*<Text style={styles.label}>Checkbox</Text>*/}
+
+                        {/*<FlatList*/}
+                        {/*    data={boxchecked}*/}
+                        {/*    renderItem={renderCheckboxItem}*/}
+                        {/*    keyExtractor={(item) => item.id.toString()}*/}
+                        {/*    horizontal={true}*/}
+                        {/*/>*/}
+
+                    </View>
+                </Modal>
 
             </View>
 
