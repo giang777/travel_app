de7769e3fd7a74c645dc16139ea14f099f3011af
Hung di code dao <101351150+hungtrinhh@users.noreply.github.com>
2023-08-06 22:04:19 +0700

    update

--- src/routes/bottom-navigation.js
+++ src/routes/bottom-navigation.js
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import HomeScreen from '../screens/home';
-import SearchScreen from '../screens/search';
-import BookingScreen from '../screens/booking';
-import ProfileScreen from '../screens/profile';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/home";
+import SearchScreen from "../screens/search";
+import BookingScreen from "../screens/booking";
+import ProfileScreen from "../screens/profile";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 const BottomBar = createBottomTabNavigator();
@@ -14,56 +14,60 @@ const BottomNavigation = () => {
     <BottomBar.Navigator
       initialRouteName="HomeScreen"
       screenOptions={{
-        tabBarActiveTintColor: '#32CD32',
+        tabBarActiveTintColor: "#32CD32",
       }}
     >
       <BottomBar.Screen
         component={HomeScreen}
-        name='HomeScreen'
+        name="HomeScreen"
         options={{
-          tabBarLabel: 'Home',
+          tabBarLabel: "Home",
           tabBarIcon: ({ color, size }) => (
             <Icon name="home" color={color} size={size} />
           ),
           headerShown: false,
-        }} />
+        }}
+      />
 
       <BottomBar.Screen
         component={SearchScreen}
-        name='SearchScreen'
+        name="SearchScreen"
         options={{
-          tabBarLabel: 'Search',
+          tabBarLabel: "Search",
           tabBarIcon: ({ color, size }) => (
             <Icon name="search" color={color} size={size} />
           ),
           headerShown: false,
-        }} />
+        }}
+      />
 
       <BottomBar.Screen
         component={BookingScreen}
-        name='BookingScreen'
+        name="BookingScreen"
         options={{
-          tabBarLabel: 'Booking',
+          tabBarLabel: "Booking",
           tabBarIcon: ({ color, size }) => (
             <Icon name="event-note" color={color} size={size} />
           ),
           headerShown: false,
-        }} />
+        }}
+      />
 
       <BottomBar.Screen
         component={ProfileScreen}
-        name='ProfileScreen'
+        name="ProfileScreen"
         options={{
-          tabBarLabel: 'Profile',
+          tabBarLabel: "Profile",
           tabBarIcon: ({ color, size }) => (
             <FontAwesome5 name="user" color={color} size={size} />
           ),
           headerShown: false,
-        }} />
+        }}
+      />
     </BottomBar.Navigator>
-  )
-}
+  );
+};
 
-export default BottomNavigation
+export default BottomNavigation;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
